55a2c54ad4a5279481462873be7fea4fce124ff1
SamiraRMG <samiramartins130@gmail.com>
2025-05-28 17:47:59 +0000

    Aula 28/05

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/', (request, response) => {
+    let resultado = [0, 1];
+    for(let cont = 2; cont <= 20; cont++) {
+        resultado.push(resultado[cont - 1] + resultado[cont - 2]);
+    }
+    response.render('home', {'valores' : resultado});
+});
+
+app.listen(3000, () => {
+    console.log('Servidor na porta 3000');
+});
